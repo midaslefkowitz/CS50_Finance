205bfa29b20342c2f037261417b08e3791266b1b
Yitz Lefkowitz <YitzLefkowitz@gmail.com>
2014-12-18 05:34:07 -0500

    Finish registeration

--- pset7/public/register.php
+++ pset7/public/register.php
@@ -6,7 +6,18 @@
     // if form was submitted
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        
+        $result = query("INSERT INTO users (username, hash, cash) VALUES(?, ?, 10000.00)", $_POST["username"], crypt($_POST["password"]));
+        if ($result === false)
+        {
+        	apologize("Sorry, somebody took that username before you. Better Luck next time.");
+        }
+        else
+        {
+        	$rows = query("SELECT LAST_INSERT_ID() AS id");
+        	$id = $rows[0]["id"];
+        	$_SESSION["id"] = $id;
+        	render("index.php");
+        }
     }
     else
     {
