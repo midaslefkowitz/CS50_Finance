b3ba70bcd2720df0d730d6b46cc03bb0bd3b60c3
Yitz Lefkowitz <YitzLefkowitz@gmail.com>
2014-12-18 06:55:50 -0500

    Highlight current page in backend header

--- pset7/templates/backend_header.php
+++ pset7/templates/backend_header.php
@@ -26,15 +26,17 @@
 
             <div id="top">
                 <a href="/"><img alt="C$50 Finance" src="/img/logo.gif"/></a>
+            	
+            	<ul class="nav nav-pills">
+		        	<li role="presentation" class="active"><a href="portfolio.php">Portfolio</a></li>
+					<li role="presentation"><a href="quote.php">Quote</a></li>
+					<li role="presentation"><a href="buy.php">Buy</a></li>
+					<li role="presentation"><a href="sell.php">Sell</a></li>
+					<li role="presentation"><a href="history.php">History</a></li>
+					<li role="presentation"><a href="logout.php"><strong>Log Out</strong></a></li>
+				</ul>
             </div>
             
-            <ul class="nav nav-pills">
-            	<li><a href="portfolio.php">Portfolio</a></li>
-				<li><a href="quote.php">Quote</a></li>
-				<li><a href="buy.php">Buy</a></li>
-				<li><a href="sell.php">Sell</a></li>
-				<li><a href="history.php">History</a></li>
-				<li><a href="logout.php"><strong>Log Out</strong></a></li>
-			</ul>
+            
 
             <div id="middle">
--- pset7/templates/portfolio.php
+++ pset7/templates/portfolio.php
@@ -1,6 +1,4 @@
 <div>
     <img alt="Under Construction" src="/img/construction.gif"/>
 </div>
-<div>
-    <a href="logout.php">Log Out</a>
-</div>
+
